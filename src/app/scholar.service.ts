c9be5100458c5cdb3792be40529dc337c23edbf5
Onno Haldar <onno@zgw.nu>
2019-12-28 15:06:33 +0100

    refactor

--- src/app/scholar.service.ts
+++ src/app/scholar.service.ts
@@ -9,7 +9,7 @@ export class ScholarService {
   constructor(private http: HttpClient) { }
 
   getLabels() {
-    const labelsUrl = '/scholar/citations?view_op=manage_labels&hl=nl&oi=srp';
+    const labelsUrl = 'https://scholar/citations?view_op=manage_labels&hl=nl&oi=srp';
 
     return this.http.get(labelsUrl, {responseType: 'text'});
   }
