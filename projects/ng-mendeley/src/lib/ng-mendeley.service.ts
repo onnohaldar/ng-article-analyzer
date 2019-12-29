c0a76b755efbe801bcd309a9c8f3f367c3160e45
Onno Haldar <onno@zgw.nu>
2019-12-29 17:33:38 +0100

    refactor

--- projects/ng-mendeley/src/lib/ng-mendeley.service.ts
+++ projects/ng-mendeley/src/lib/ng-mendeley.service.ts
@@ -1,12 +1,12 @@
 // Angular
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgMendeleyService {
-  readonly apiUrl = 'https://api.mendeley.com/';
+  readonly apiUrl = 'https://api.mendeley.com';
   private authParms = {
     accessToken: '<ACCESS_TOKEN>'
   };
@@ -17,16 +17,26 @@ export class NgMendeleyService {
     this.authParms.accessToken = value;
   }
 
-  buildHeaders(acceptValue: string) {
-    return {
-      Authorization: 'Bearer ' + this.authParms.accessToken,
-      Accept: acceptValue
-    };
+  buildUrl(method: string, id?: string) {
+    if (id) { method += '/' + id; }
+    return this.apiUrl + '/' + method;
+  }
+
+  get authToken() {
+    return 'Bearer ' + this.authParms.accessToken;
+  }
+
+  get(method: string, returnType: string, id?: string, params?: HttpParams) {
+    return this.http.get(this.buildUrl(method),
+      { headers: {
+        Authorization: this.authToken,
+        Accept: 'application/vnd.mendeley-' + returnType
+      },
+      params });
   }
 
   get userRoles() {
-    return this.http.get(this.apiUrl + 'user_roles',
-      { headers: this.buildHeaders('application/vnd.mendeley-user-role.1+json') });
+    return this.get('user_roles', 'user-role.1+json');
   }
 
 }
