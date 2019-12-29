d8c24dc65d672eab7d0f93fac3476fa7f2c8981c
Onno Haldar <onno@zgw.nu>
2019-12-29 19:01:56 +0100

    refactor

--- src/app/mendeley/mendeley.component.ts
+++ src/app/mendeley/mendeley.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // Libraries
-import { NgMendeleyService } from 'ng-mendeley';
+import { NgMendeleyService, NgMendeleyDocumentsService } from 'ng-mendeley';
 
 @Component({
   selector: 'app-mendeley',
@@ -11,7 +11,9 @@ import { NgMendeleyService } from 'ng-mendeley';
 })
 export class MendeleyComponent implements OnInit {
 
-  constructor(private service: NgMendeleyService) { }
+  constructor(
+    private service: NgMendeleyService,
+    private documentsService: NgMendeleyDocumentsService) { }
 
   ngOnInit() {
     this.service.accessToken = 'MSwxNTc3NjQyNTEwNTE2LDU2MTMxMjY3MSwxMDI4LGFsbCwsLDg3MDAyNjllNzQzZjc0NDE3YTk5OWJkMjU1ZTMzMGE3NjdhNmd4cnFiLGYxZTRlZTM4LWEyZjUtMzQ2Yy04YTViLWExNzE1MjYwOThkMCxhUU92Q1dkWkVsQm5PbGRIQ0VCd1BMekNVb00';
@@ -19,6 +21,11 @@ export class MendeleyComponent implements OnInit {
       roles => console.log(roles),
       error => console.log(error),
       () => console.log('userRoles done!')
+    );
+    this.documentsService.get().subscribe(
+      docs => console.log(docs),
+      error => console.log(error),
+      () => console.log('getDocs done!')
     );
   }
 
