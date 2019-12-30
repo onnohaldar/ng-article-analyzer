86fc9985f24f829888a53f536100ac3cfb8dcd3e
Onno Haldar <onno@zgw.nu>
2019-12-30 12:23:25 +0100

    refactor

--- projects/ng-mendeley/src/lib/ng-mendeley-folders.service.ts
+++ projects/ng-mendeley/src/lib/ng-mendeley-folders.service.ts
@@ -10,8 +10,8 @@ import { NgMendeleyService } from './ng-mendeley.service';
 interface Folder {
   id: string;	// id of the folder
   name: string;	// name of the folder
-  parent_id: string;	// id of the parent folder
-  group_id: string;	// id of the owning group
+  parent_id?: string;	// id of the parent folder
+  group_id?: string;	// id of the owning group
   created: string;	// date the folder was created. This date is represented in ISO 8601 format.
   modified: string;	// date the folder was modified. This date is represented in ISO 8601 format.
 }
--- src/app/mendeley/mendeley.component.ts
+++ src/app/mendeley/mendeley.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // Libraries
-import { NgMendeleyService, NgMendeleyDocumentsService } from 'ng-mendeley';
+import { NgMendeleyService, NgMendeleyDocumentsService, NgMendeleyFoldersService } from 'ng-mendeley';
 
 @Component({
   selector: 'app-mendeley',
@@ -13,19 +13,25 @@ export class MendeleyComponent implements OnInit {
 
   constructor(
     private service: NgMendeleyService,
-    private documentsService: NgMendeleyDocumentsService) { }
+    private documentsService: NgMendeleyDocumentsService,
+    private foldersService: NgMendeleyFoldersService) { }
 
   ngOnInit() {
     this.service.accessToken = 'MSwxNTc3NzA1MDI3ODkyLDU2MTMxMjY3MSwxMDI4LGFsbCwsLDQ3Yjc3YTNhNjM5YWM1NDU5ZjE5YjRmMjRhMDE1NzdhMjM2Ymd4cnFiLGYxZTRlZTM4LWEyZjUtMzQ2Yy04YTViLWExNzE1MjYwOThkMCwyNl9vUjJQMjV0RDZTUmtBY3lhRzFBUm9RTm8';
-    this.service.userRoles.subscribe(
+    this.service.retrieveAllUserRoles().subscribe(
       roles => console.log(roles),
       error => console.log(error),
-      () => console.log('userRoles done!')
+      () => console.log('retrieveAllUserRoles done!')
     );
-    this.documentsService.retrieveADoc('877458db-9425-3cbc-8daf-2574ea07f162').subscribe(
+    this.documentsService.retrieveADocument('877458db-9425-3cbc-8daf-2574ea07f162').subscribe(
       doc => console.log(doc.title),
       error => console.log(error),
-      () => console.log('getDocs done!')
+      () => console.log('retrieveADocument done!')
+    );
+    this.foldersService.listAllFolders().subscribe(
+      folders => console.log(folders),
+      error => console.log(error),
+      () => console.log('listAllFolders done!')
     );
   }
 
