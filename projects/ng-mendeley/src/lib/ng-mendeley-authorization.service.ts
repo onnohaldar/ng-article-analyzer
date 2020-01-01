43ac0e2f1e22090a16dd080c3ade3f5af9038022
Onno Haldar <onno@zgw.nu>
2020-01-01 15:00:15 +0100

    refactor

--- projects/ng-mendeley/src/lib/ng-mendeley-authorization.service.ts
+++ projects/ng-mendeley/src/lib/ng-mendeley-authorization.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@angular/core';
 
 // Library
 import { NgMendeleyService } from './ng-mendeley.service';
+interface AuthParms {
+  clientId?: string;
+  redirectUri?: string;
+  responseType: string;
+  scope: string;
+  accessToken?: string;
+  refreshToken?: string;
+  clientSecret?: string;
+  readonly tokenType: string;
+  expires?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +24,7 @@ export class NgMendeleyAuthorizationService {
    * @see <https://www.oauth.com/oauth2-servers/making-authenticated-requests/refreshing-an-access-token/>
    * @see <https://dev.mendeley.com/reference/topics/authorization_auth_code.html>
    */
-  private authParms = {
-    redirectUri: undefined,
-    accessToken: undefined,
-    refreshToken: undefined,
-    clientId: undefined,
-    clientSecret: undefined,
-    tokenType: 'bearer',
-    expires: undefined
-  };
+  private authParms: AuthParms = { responseType: 'code', scope: 'all', tokenType: 'bearer' };
 
   constructor(private service: NgMendeleyService) { }
 
